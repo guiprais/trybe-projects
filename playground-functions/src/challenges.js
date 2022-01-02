a32be0e8c54b8da49fd5dde76fa8ba38d63e573b
Guilherme de Prais <guilherme.prais@gmail.com>
2022-01-02 10:45:05 -0300

    req 09: encode and decode

--- playground-functions/src/challenges.js
+++ playground-functions/src/challenges.js
@@ -64,11 +64,22 @@ function fizzBuzz(array) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  return string
+    .replaceAll('a', 1)
+    .replaceAll('e', 2)
+    .replaceAll('i', 3)
+    .replaceAll('o', 4)
+    .replaceAll('u', 5);
 }
-function decode() {
-  // seu código aqui
+
+function decode(string) {
+  return string
+    .replaceAll(1, 'a')
+    .replaceAll(2, 'e')
+    .replaceAll(3, 'i')
+    .replaceAll(4, 'o')
+    .replaceAll(5, 'u');
 }
 
 module.exports = {
